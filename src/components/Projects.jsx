167fa40bc5234df97c2523a3f7768328c0af07e6
Yashveer Singh <yashveer.rtc@gmail.com>
2023-08-11 09:21:12 +0530

    updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import Working from "../assets/projects/workImg.jpeg";
+import Amazon from "../assets/projects/amazon-clone.png";
+import Exercise from "../assets/projects/exercise-app.png";
+import Weather from "../assets/projects/weather-app.png";
+import UiUx from "../assets/projects/uiux.png";
 
 const Projects = () => {
   return (
-    <div name="projects" className="w-full h-screen flex flex-col justify-between items-center pt-28 pb-6">
+    <div
+      name="projects"
+      className="w-full h-screen flex flex-col justify-between items-center pt-28 pb-6"
+    >
       <div className="flex flex-row justify-center w-full">
         <h1 className="text-white bg-[#181818] py-3 px-8 text-2xl rounded-[0.5rem]">
           Projects
@@ -11,19 +17,24 @@ const Projects = () => {
       </div>
       <div className="max-w-[1000px] mx-auto grid sm:grid-cols-2 md:grid-cols-2 gap-6 w-full">
         <div
-          style={{ backgroundImage: `url(${Working})` }}
+          style={{ backgroundImage: `url(${Amazon})` }}
           className="group content-div bg-[#181818] p-6 rounded-[1rem] flex justify-center items-center"
         >
           <div className="opacity-0 group-hover:opacity-100 duration-500">
-            <span className="text-2xl font-bold text-white">React App</span>
-            <div className="pt-8 text-center">
+            <span className="flex text-2xl font-bold text-white justify-center">
+              Amazon Clone
+            </span>
+            <div className="pt-6 text-center">
               <a href="/">
-                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg">
+                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg hover:translate-y-[-0.4rem] duration-500">
                   Demo
                 </button>
               </a>
-              <a href="/">
-                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg">
+              <a
+                href="https://github.com/RealThomasCat/amazon-clone"
+                target="_blank"
+              >
+                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg hover:translate-y-[-0.4rem] duration-500">
                   Code
                 </button>
               </a>
@@ -31,19 +42,27 @@ const Projects = () => {
           </div>
         </div>
         <div
-          style={{ backgroundImage: `url(${Working})` }}
+          style={{ backgroundImage: `url(${Exercise})` }}
           className="group content-div bg-[#181818] p-6 rounded-[1rem] flex justify-center items-center"
         >
           <div className="opacity-0 group-hover:opacity-100 duration-500">
-            <span className="text-2xl font-bold text-white">React App</span>
-            <div className="pt-8 text-center">
-              <a href="/">
-                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg">
+            <span className="flex text-2xl font-bold text-white justify-center">
+              Exercise App
+            </span>
+            <div className="pt-6 text-center">
+              <a href="https://rtc-react-gym.netlify.app/" target="_blank">
+                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg hover:translate-y-[-0.4rem] duration-500">
                   Demo
                 </button>
               </a>
-              <a href="/">
-                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg">
+              <a
+                href="https://github.com/RealThomasCat/gym-app"
+                target="_blank"
+              >
+                <button
+                  className="text-center rounded-[1rem] text-white 
+                bg-[#181818] px-4 py-3 m-2 font-bold text-lg hover:translate-y-[-0.4rem] duration-500"
+                >
                   Code
                 </button>
               </a>
@@ -51,19 +70,24 @@ const Projects = () => {
           </div>
         </div>
         <div
-          style={{ backgroundImage: `url(${Working})` }}
+          style={{ backgroundImage: `url(${Weather})` }}
           className="group content-div bg-[#181818] p-6 rounded-[1rem] flex justify-center items-center"
         >
           <div className="opacity-0 group-hover:opacity-100 duration-500">
-            <span className="text-2xl font-bold text-white">React App</span>
-            <div className="pt-8 text-center">
+            <span className="flex text-2xl font-bold text-white justify-center">
+              Weather App
+            </span>
+            <div className="pt-6 text-center">
               <a href="/">
-                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg">
+                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg hover:translate-y-[-0.4rem] duration-500">
                   Demo
                 </button>
               </a>
-              <a href="/">
-                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg">
+              <a
+                href="https://github.com/RealThomasCat/Weather-app"
+                target="_blank"
+              >
+                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg hover:translate-y-[-0.4rem] duration-500">
                   Code
                 </button>
               </a>
@@ -71,20 +95,22 @@ const Projects = () => {
           </div>
         </div>
         <div
-          style={{ backgroundImage: `url(${Working})` }}
+          style={{ backgroundImage: `url(${UiUx})` }}
           className="group content-div bg-[#181818] p-6 rounded-[1rem] flex justify-center items-center"
         >
           <div className="opacity-0 group-hover:opacity-100 duration-500">
-            <span className="text-2xl font-bold text-white">React App</span>
-            <div className="pt-8 text-center">
-              <a href="/">
-                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg">
-                  Demo
+            <span className="flex text-2xl font-bold text-white justify-center">
+              UI/UX
+            </span>
+            <div className="pt-6 text-center">
+              <a href="https://dribbble.com/Real_Thomas_Cat" target="_blank">
+                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg hover:translate-y-[-0.4rem] duration-500">
+                  Dribbble
                 </button>
               </a>
-              <a href="/">
-                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg">
-                  Code
+              <a href="https://www.behance.net/yashveersingh8" target="_blank">
+                <button className="text-center rounded-[1rem] text-white bg-[#181818] px-4 py-3 m-2 font-bold text-lg hover:translate-y-[-0.4rem] duration-500">
+                  Behance
                 </button>
               </a>
             </div>
